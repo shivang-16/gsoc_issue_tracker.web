d04035546c026e406815179dd7cbf41ed217e4da
shivang-16 <shivangyadav121@gmail.com>
2025-01-03 01:45:37 +0530

    fix build error

--- src/app/(root)/gsoc-issues/page.tsx
+++ src/app/(root)/gsoc-issues/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Issues from '../_components/Issues'
 
 const page = () => {
+    const [issueFilters, setIssueFilters] = useState({
+        state: [],
+        label: [],
+        organizations: [],
+        });
+
   return (
     <div className='h-full overflow-auto m-auto pt-20'>
-        <Issues />
+        <Issues filters={issueFilters}/>
     </div>
   )
 }
